9f1191b049bf4c142dab37a416eca0647d665401
Phoenix Do <777dophoenix@gmail.com>
2021-01-23 13:06:51 -0500

    Changed config-exports.js to reflect personal values

--- processors/GetIntradayPrices/config-exports.js
+++ processors/GetIntradayPrices/config-exports.js
@@ -1,10 +1,10 @@
 const config = {
-    cognito_pool_id: "eu-west-1_DMhZ3rrXS",
-    cognito_backend_client_id: "pq3rcra4kend08leir0lbth06",
-    cognito_backend_access_key: "stockMonitoring/local/backend",
-    appsync_endpoint_url: "https://efrgcmtrnfcn7oshfxf2kvsx4y.appsync-api.eu-west-1.amazonaws.com/graphql",
-    appsync_region: "eu-west-1",
-    data_feed_key: "stockMonitoring/local/datafeed",
+    cognito_pool_id: "us-east-1_3pMvSxqD0",
+    cognito_backend_client_id: "7nner5n071u914r0uhcluj63t0",
+    cognito_backend_access_key: "stockDashboard/master/backend",
+    appsync_endpoint_url: "https://cirf3p5wkrgx7acztbehwc5m5i.appsync-api.us-east-1.amazonaws.com/graphql",
+    appsync_region: "us-east-1",
+    data_feed_key: "stockDashboard/master/datafeed",
     feed_interval: "30min"
 }
 
