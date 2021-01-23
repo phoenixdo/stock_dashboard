19e289362f8cdb76b579f1d82c24cd0edd1201ad
Phoenix Do <777dophoenix@gmail.com>
2021-01-23 15:42:06 -0500

    Changed config again to update endpoints

--- processors/GetIntradayPrices/config-exports.js
+++ processors/GetIntradayPrices/config-exports.js
@@ -1,10 +1,10 @@
 const config = {
-    cognito_pool_id: "us-east-1_3pMvSxqD0",
-    cognito_backend_client_id: "7nner5n071u914r0uhcluj63t0",
-    cognito_backend_access_key: "stockDashboard/master/backend",
-    appsync_endpoint_url: "https://cirf3p5wkrgx7acztbehwc5m5i.appsync-api.us-east-1.amazonaws.com/graphql",
+    cognito_pool_id: "us-east-1_mCON86rxr",
+    cognito_backend_client_id: "7haf0afmmu1er6v2ov8i5lj2fa",
+    cognito_backend_access_key: "stockdashboard/master/backend",
+    appsync_endpoint_url: "https://khnhpkqfi5ekblek2jzdkro7yi.appsync-api.us-east-1.amazonaws.com/graphql",
     appsync_region: "us-east-1",
-    data_feed_key: "stockDashboard/master/datafeed",
+    data_feed_key: "stockdashboard/master/datafeed",
     feed_interval: "30min"
 }
 
